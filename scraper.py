f809f0c896c513798f1e40ed8391491d984c5196
L.Vella <lorenzo.vella@live.com>
2021-06-18 16:34:16 -0300

    auto resume bug fixes and .exe

--- scraper.py
+++ scraper.py
@@ -1,6 +1,7 @@
 import csv
 import selenium
 import random
+import time
 #import logging
 import sys
 from seleniumwire import webdriver
@@ -20,12 +21,10 @@ class Scraper():
                 'http': self.proxy()
             }
         }
-        #options.add_experimental_option('excludeSwitches', ['enable-logging'])
-        self.driver = Chrome(ChromeDriverManager().install(), options=options, seleniumwire_options=wireoptions)
+        self.driver = Chrome(self.chrome(), options=options, seleniumwire_options=wireoptions)
         self.driver.set_page_load_timeout(30)
-
     def data(self, q):
-        self.driver.get('https://www.google.com.br/search?q='+q+" hotel")
+        self.driver.get('https://www.google.com/search?q='+q+" hotel")
         if self.googleBlock():
             self.resetProxy();
         return {
@@ -34,31 +33,26 @@ class Scraper():
             'url': self.url(),
             'newName': self.newName()
             }
-
     def phone(self):
         try:
             return self.driver.find_element_by_css_selector("[data-attrid='kc:/collection/knowledge_panels/has_phone:phone']>div>div>span:last-of-type>span>a>span").get_attribute('innerHTML')
         except selenium.common.exceptions.NoSuchElementException:
             return ""
-
     def address(self):
         try:
             return self.driver.find_element_by_css_selector("[data-attrid='kc:/location/location:address']>div>div>span:last-of-type").get_attribute('innerHTML')
         except selenium.common.exceptions.NoSuchElementException:
             return ""
-
     def url(self):
         try:
             return self.driver.find_element_by_css_selector("[rel='prerender']").get_attribute('href')
         except selenium.common.exceptions.NoSuchElementException:
             return ""
-
     def newName(self):
         try:
             return self.driver.find_element_by_css_selector("[data-attrid='title']>span").get_attribute('innerHTML')
         except selenium.common.exceptions.NoSuchElementException:
             return ""
-
     def googleBlock(self):
         try:
             self.driver.find_element_by_css_selector("html[itemtype='http://schema.org/SearchResultsPage']")
@@ -66,38 +60,43 @@ class Scraper():
         except selenium.common.exceptions.NoSuchElementException:
             return True
     def resetProxy(self):
-        print("Atualizando proxy")
-        newProxy = self.proxy().split(':')
-        self.driver.proxy._master.options.update(
-            mode="upstream:"+(":").join((newProxy[0],newProxy[1],newProxy[-1])),
-            upstream_auth=(":").join(newProxy[2].split("@"))
-        )
-    def close(self):
         self.driver.close()
+        self.__init__()
+        # newProxy = self.proxy().split(':')
+        # self.driver.proxy._master.options.update(
+        #     mode="upstream:"+(":").join((newProxy[0],newProxy[1],newProxy[-1])),
+        #     upstream_auth=(":").join(newProxy[2].split("@"))
+        # )
+        raise Exception("Atualizando proxy")
+    def chrome(self):
+        try:
+            return ChromeDriverManager().install()
+        except Exception as e:
+            print(e)
+            time.sleep(5)
+            return self.chrome()
+
 def runReader(startFrom=0):
-    scraperInstance = Scraper()
-    try:
-        for row in csvReader:
+    with open(inputFile) as csvDataFile:
+        csvReader = csv.DictReader(csvDataFile, delimiter=csvDelimiter)
+        for index, row in enumerate(csvReader):
+            if index <= startFrom-1:
+                continue
             scrapedData = scraperInstance.data(row[hotelNameCol])
             newData.append(scrapedData)
-            print("Processando "+str(len(newData))+" de "+numberOfLines+" | "+scrapedData['newName'][:15]+"... | "+scrapedData['address'][:15]+"... | "+scrapedData['url'][:15]+"... | "+scrapedData['phone'][:15])
-        add_column_in_csv(inputFile, inputFile.replace('.','Output-'+str(len(newData))+'.',1),startFrom)
-    except Exception as e:
-        print(e)
-        print("Retomando conversão a partir da linha "+str(len(newData)))
-        scraperInstance.close()
-        runReader(len(newData))
-def add_column_in_csv(input_file, output_file, startFrom=0):
-    with open(input_file, 'r') as read_obj, \
+            print("Processando "+str(len(newData))+" de "+str(numberOfLines)+" | "+scrapedData['newName'][:15]+"... | "+scrapedData['address'][:15]+"... | "+scrapedData['url'][:15]+"... | "+scrapedData['phone'][:15])
+
+def add_column_in_csv(input_file_param, output_file):
+    with open(input_file_param, 'r') as read_obj, \
             open(output_file, 'w', newline='', encoding="utf-8") as write_obj:
         csv_reader = csv.reader(read_obj, delimiter=csvDelimiter)
         csv_writer = csv.writer(write_obj, delimiter=csvDelimiter)
         for row in csv_reader:
-            if csv_reader.line_num-2 < len(newData) and csv_reader.line_num-2 >= startFrom:
+            if csv_reader.line_num-2 < len(newData):
                 transform_row(row, csv_reader.line_num)
                 csv_writer.writerow(row)
-    newData.clear()
     print("Arquivo "+output_file+" convertido com sucesso.")
+
 def transform_row(row, lineNum):
     if lineNum == 1:
         row.extend(['phone','address','url','newName'])
@@ -108,20 +107,23 @@ def transform_row(row, lineNum):
             newData[lineNum-2]['url'],
             newData[lineNum-2]['newName']
         ])
-
 newData = []
 csvDelimiter = ";"
 hotelNameCol = 'name'
-
+scraperInstance = Scraper()
 while(True):
     inputFile = input("Nome arquivo original: ")
     if inputFile != "":
-        try:
-            numberOfLines = str(sum(1 for line in open(inputFile))-1)
-            with open(inputFile) as csvDataFile:
-                csvReader = csv.DictReader(csvDataFile, delimiter=csvDelimiter)
-                runReader()
-        except (Exception, KeyboardInterrupt) as e:
-            print(e)
-            print("Houve um erro e a pesquisa foi salva com "+str(len(newData))+" resultados")
-            add_column_in_csv(inputFile, inputFile.replace('.','Output-'+str(len(newData))+'.',1))
+        numberOfLines = sum(1 for line in open(inputFile))-1
+        while len(newData) != numberOfLines:
+            try:
+                runReader(len(newData))
+            except Exception as e:
+                print(e)
+                print("Retomando conversão a partir da linha "+str(len(newData)))
+            except KeyboardInterrupt as e:
+                print("Pesquisa interrompida e salva com "+str(len(newData))+" resultados")
+                # add_column_in_csv(inputFile, inputFile.replace('.','OutputRestored-'+str(len(newData))+'.',1))
+                break
+        add_column_in_csv(inputFile, inputFile.replace('.','Output-'+str(len(newData))+'.',1))
+        newData.clear()
